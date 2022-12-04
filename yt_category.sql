4e12ffd0d203901d2598036bed1b5e6b18cb8ce1
Luis Figueroa Castillo <116255679+apariciodevcom@users.noreply.github.com>
2022-12-04 12:49:23 +0100

    Delete yt_category.sql

--- yt_category.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.yt_category
-(
-    category_id serial NOT NULL,
-    category_name character varying(50) NOT NULL,
-    PRIMARY KEY (category_id)
-);
-
-ALTER TABLE IF EXISTS public.yt_category
-    OWNER to postgres;
